e5697025821c1b95367d3b00c04e39003ec4a433
Jonatas <contact@jonatasoliveira.dev>
2021-10-30 14:25:41 -0300

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from search.api import search
 from user.api import user
 
 
 def create_app():
+
     app = FastAPI()
+
+    origins = [
+        "http://localhost",
+        "http://localhost:8080",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     app.include_router(search)
     app.include_router(user)
     return app
